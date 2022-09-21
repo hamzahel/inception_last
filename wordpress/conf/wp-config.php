c24d8fec345f04706e6af4bdb98d2fd6408470af
hamzahel <elayach.hamza.work@gmail.com>
2022-09-21 18:44:45 -0400

    update007

--- wordpress/conf/wp-config.php
+++ wordpress/conf/wp-config.php
@@ -20,22 +20,22 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define('DB_USER', 'alfrido');
+define( 'DB_USER', 'alfrido' );
 
 /** Database password */
-define('DB_PASSWORD', '123qwe');
+define( 'DB_PASSWORD', '123qwe' );
 
 /** Database hostname */
-define('DB_HOST', 'mariadb');
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication unique keys and salts.
@@ -48,38 +48,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define(
-    'AUTH_KEY',
-    'lmQfNZg>;XBW(;p$YMT+y=+%xxY}_+.tfyHaGfU8=BN^Y=rOW@OK.<P4EHF-:%B!'
-);
-define(
-    'SECURE_AUTH_KEY',
-    'Bn)(hAVml0:l|2-bdwfS9|l>:brLwzmU(0f^_`o!n_nvz,R3]^ )K=-/7c9{:?=3'
-);
-define(
-    'LOGGED_IN_KEY',
-    '&pOx?)Ya=%OWR4w$&X-sv2eW%9+Y0|YHKRiU0*|d8d4&U^%i;{,;+:`~am5k3Kl>'
-);
-define(
-    'NONCE_KEY',
-    '^@@8o:1-D5>|whK-7fK-+wGVEs@,R_azJF`m/~lal=gDgg4*lUH`7<tNx St[&w0'
-);
-define(
-    'AUTH_SALT',
-    'TX{_QCi7Y$90FV+[[-)E&FQHksJ3~0Rp^gv>ip08qLLz*d-VO|hG}x5Z1rdY?Ohl'
-);
-define(
-    'SECURE_AUTH_SALT',
-    'eCwU>y+?SQ+eKw31_ JZ3x cM)s]m@f;si<Oy7#TAlW9^Am|d/K(!GpFuia7+{+-'
-);
-define(
-    'LOGGED_IN_SALT',
-    ')DAOFh IdnE0~N+tXhKn|C~?F(%gmkEg/.B`jq%|C;#:Jh2;7]mO-s~_#-XB$3G*'
-);
-define(
-    'NONCE_SALT',
-    'talVH@[R34q8>MyyZg}-e*#{WUCG+E]z4W,rSA]X6gDJ5f5yc uoKM-{mhWP-6O_'
-);
+define( 'AUTH_KEY',         'qR%s{y*~[^4NvXZkht``6YFAno^(w>kpq}$qU-;I,nt/!Ej^Abr!Fw/vLm%~R#[D' );
+define( 'SECURE_AUTH_KEY',  '`2QmeU6_E}+n@JXtinDy:? 2[*O>33} 9,quc+7R=@!_,YC^;f7@Y)M]j(<i3_$l' );
+define( 'LOGGED_IN_KEY',    'vk:Om91+PBM|zF=fC/%)Pv6KUNKFw$t]Q)q7JHOW>GHdv:T|owI[VcCKMLgoP1cU' );
+define( 'NONCE_KEY',        'Nw%/_iJmEF{mS+Od!W?{qJ.A3?--ex^43g>xk85uKyDa$YF)wIS%SP#O+Y Hx<-<' );
+define( 'AUTH_SALT',        '2<rCmsK{kb,3Xx_YpbyEX1f?hW1_1uK& FOB#ROED)_w8sd8QX/LH<K+{@zJgYH:' );
+define( 'SECURE_AUTH_SALT', ' uO.TptL;42?Drf0]rwrB)tiW<3?;*;_D=SG}&]  G^e?v_@Gj.ZAH.HqUW+l^wm' );
+define( 'LOGGED_IN_SALT',   'LF)(;i$ o%=,?ZcQ8kq`>`L_6#Gn4u8n[#r/Z{K=0$%c cG8mI0#0_)mH!F<^A,1' );
+define( 'NONCE_SALT',       'CLv}:iHul<7_swjQDIqY($dY8mY}m::7;G8>:CI$GX3%jg m)8(5/~ +-G!l6fJ+' );
 
 /**#@-*/
 
@@ -89,7 +65,7 @@ define(
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wp_';
+$table_prefix = 'wp_alfrido';
 
 /**
  * For developers: WordPress debugging mode.
@@ -103,16 +79,19 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define('WP_DEBUG', false);
+define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
+
+
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if (!defined('ABSPATH')) {
-    define('ABSPATH', __DIR__ . '/');
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
--- wordpress/tools/wp.sh
+++ wordpress/tools/wp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /var/www/html/wp-config.php ]
+if [ ! -f /var/ww/html/wp-config.php ]
 then
 	mkdir -p /var/www/html;
 	chmod -R www-data:www-data /var/www/html
@@ -9,10 +9,13 @@ then
 	mv wp-cli.phar /usr/local/bin/wp;
 	cd /var/www/html;
 	wp core download --allow-root;
-	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${ANIME_NAME} --admin_password=${AMINE_PASS} --admin_email=${ADMIN_EMAIL};
+fi
+cp /tmp/wp-config.php /var/www/html/wp-config.php;
+rm /var/www/html/wp-config-sample.php >/dev/null
+if [ -f /var/www/html/wp-config.php ]
+then
+	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${ADMIN_NAME} --admin_password=${ADMIN_PASS} --admin_email=${ADMIN_EMAIL};
 	wp user create --allow-root ${USER_NAME} ${USER_EMAIL} --user_pass=${USER_PASS};
-	mv /tmp/wp-config.php /var/www/html/wp-config.php;
-	rm /var/www/html/wp-config-sample.php > /dev/null;
 fi
 
 echo "$@"
